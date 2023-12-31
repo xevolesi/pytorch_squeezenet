19e6b5fcff7763a368e36688263a0f62863eb765
Mark Strakhov <easytomalbolge@gmail.com>
2023-12-31 18:21:56 +0300

    Add fire module factory to squeezenet

--- source/models/squeezenet.py
+++ source/models/squeezenet.py
@@ -1,29 +1,39 @@
 import torch
 from torch import nn
 
-from source.modules import FireModule
+from source.modules import fire_module_factory
 
 from .utils import do_paper_initialization
 
 
 class SqueezeNetV1(nn.Module):
-    def __init__(self, in_channels: int = 3, num_classes: int = 1000) -> None:
+    def __init__(self, in_channels: int = 3, num_classes: int = 1000, skip_connection_type: str | None = None) -> None:
         super().__init__()
+
+        simple_skips = None
+        complex_skips = None
+        if skip_connection_type == "simple":
+            simple_skips = "simple"
+            complex_skips = None
+        elif skip_connection_type == "complex":
+            simple_skips = "simple"
+            complex_skips = "complex"
+
         self.in_channels = in_channels
         self.features = nn.Sequential(
             nn.Conv2d(in_channels=self.in_channels, out_channels=96, kernel_size=(7, 7), stride=(2, 2)),
             nn.ReLU(inplace=True),
             nn.MaxPool2d((3, 3), stride=(2, 2), ceil_mode=True),
-            FireModule(96, 16, 128),
-            FireModule(128, 16, 128, apply_bypass_connections=True),
-            FireModule(128, 32, 256),
+            fire_module_factory(96, 16, 128, complex_skips),
+            fire_module_factory(128, 16, 128, simple_skips),
+            fire_module_factory(128, 32, 256, complex_skips),
             nn.MaxPool2d((3, 3), stride=2, ceil_mode=True),
-            FireModule(256, 32, 256, apply_bypass_connections=True),
-            FireModule(256, 48, 384),
-            FireModule(384, 48, 384, apply_bypass_connections=True),
-            FireModule(384, 64, 512),
+            fire_module_factory(256, 32, 256, simple_skips),
+            fire_module_factory(256, 48, 384, complex_skips),
+            fire_module_factory(384, 48, 384, simple_skips),
+            fire_module_factory(384, 64, 512, complex_skips),
             nn.MaxPool2d((3, 3), stride=2, ceil_mode=True),
-            FireModule(512, 64, 512, apply_bypass_connections=True),
+            fire_module_factory(512, 64, 512, simple_skips),
             nn.Dropout(0.5),
         )
         self.final_conv = nn.Sequential(
@@ -38,23 +48,33 @@ class SqueezeNetV1(nn.Module):
 
 
 class SqueezeNetV11(nn.Module):
-    def __init__(self, in_channels: int = 3, num_classes: int = 1000) -> None:
+    def __init__(self, in_channels: int = 3, num_classes: int = 1000, skip_connection_type: str | None = None) -> None:
         super().__init__()
+
+        simple_skips = None
+        complex_skips = None
+        if skip_connection_type == "simple":
+            simple_skips = "simple"
+            complex_skips = None
+        elif skip_connection_type == "complex":
+            simple_skips = "simple"
+            complex_skips = "complex"
+
         self.in_channels = in_channels
         self.features = nn.Sequential(
             nn.Conv2d(in_channels=self.in_channels, out_channels=64, kernel_size=(3, 3), stride=(2, 2)),
             nn.ReLU(inplace=True),
             nn.MaxPool2d((3, 3), stride=(2, 2), ceil_mode=True),
-            FireModule(64, 16, 128),
-            FireModule(128, 16, 128, apply_bypass_connections=False),
+            fire_module_factory(64, 16, 128, complex_skips),
+            fire_module_factory(128, 16, 128, simple_skips),
             nn.MaxPool2d((3, 3), stride=2, ceil_mode=True),
-            FireModule(128, 32, 256),
-            FireModule(256, 32, 256, apply_bypass_connections=False),
+            fire_module_factory(128, 32, 256, complex_skips),
+            fire_module_factory(256, 32, 256, simple_skips),
             nn.MaxPool2d((3, 3), stride=2, ceil_mode=True),
-            FireModule(256, 48, 384),
-            FireModule(384, 48, 384, apply_bypass_connections=False),
-            FireModule(384, 64, 512),
-            FireModule(512, 64, 512, apply_bypass_connections=False),
+            fire_module_factory(256, 48, 384, complex_skips),
+            fire_module_factory(384, 48, 384, simple_skips),
+            fire_module_factory(384, 64, 512, complex_skips),
+            fire_module_factory(512, 64, 512, simple_skips),
             nn.Dropout(0.5),
         )
         self.final_conv = nn.Sequential(
